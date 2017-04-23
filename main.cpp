2172a0aef2a318c78cd03e377c14c748888f3929
Ilshat Galimov <ilshatgalimovf@gmail.com>
2017-04-23 19:18:31 +0300

    First lab was done

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+/*
+ * Функция, возвращающая количество пересечений с осью OX (решений).
+ */
+int solutionsCount(int a, int b, int c) {
+    double d = pow(b, 2) - 4*a*c;
+    if (d < 0) return 0;
+    if (d == 0) return 1;
+    return 2;
+}
+
+int main() {
+    setlocale(LC_ALL, "rus");
+
+    int a, b, c;
+    cout << "Введите a, b и c.\n";
+    cin >> a >> b >> c;
+    if (a > 0)
+        cout << "\nВетви параболы направлены вверх.\n";
+    else cout << "\nВетви параболы направлены вниз.\n";
+    cout << "\nКоличество точек пересечения с осью OX = " << solutionsCount
+            (a, b, c) << endl;
+
+    return 0;
+}
